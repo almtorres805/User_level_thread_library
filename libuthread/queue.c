83892715a99bff78b742df3fa249958557ccbbfb
almtorres805 <almtorres805@gmail.com>
2023-05-04 04:36:33 -0700

    implement create,enqueue,dequeue functions

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -4,13 +4,27 @@
 
 #include "queue.h"
 
+struct node {
+  void *data;
+  struct node *next;
+};
+
 struct queue {
-	/* TODO Phase 1 */
+  /* TODO Phase 1 */
+  struct node *front;
+  struct node *rear;
 };
 
 queue_t queue_create(void)
 {
-	/* TODO Phase 1 */
+  /* TODO Phase 1 */
+  queue_t q = malloc(sizeof(*q));
+  if (q == NULL){
+    return NULL;
+  }
+  q->front = NULL;
+  q->rear = NULL;
+  return q;
 }
 
 int queue_destroy(queue_t queue)
@@ -21,11 +35,45 @@ int queue_destroy(queue_t queue)
 int queue_enqueue(queue_t queue, void *data)
 {
 	/* TODO Phase 1 */
+  if (queue == NULL | data == NULL) {
+    return -1;
+  }
+  
+  struct node *n = malloc(sizeof(*n));
+  // if failure to allocate a new node
+  if (n == NULL) {
+    return -1;
+  }
+
+  n->data = data;
+  n->next = NULL;
+  if (queue->rear == NULL) {
+    queue->rear = n;
+    queue->front = n;
+  } else {
+    queue->rear->next = n;
+    queue->rear = n;
+  }
+  return 0;
 }
 
 int queue_dequeue(queue_t queue, void **data)
 {
 	/* TODO Phase 1 */
+  if (queue == NULL | data == NULL | queue->front == NULL) {
+    return -1;
+  }
+  
+  *data = queue->front->data;
+
+  struct node *tmp = queue->front;
+  queue->front = queue->front->next;
+  free(tmp);
+
+  if (queue->front == NULL) {
+    queue->rear = NULL;
+  }
+  return 0;
 }
 
 int queue_delete(queue_t queue, void *data)
@@ -42,4 +90,3 @@ int queue_length(queue_t queue)
 {
 	/* TODO Phase 1 */
 }
-
